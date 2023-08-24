1917f6f946ba1e522a7a3586021888f0eb767a4c
Stephen <stephen.gloade@gmail.com>
2023-08-24 12:50:58 -0400

    server setup

--- workoutlog/server/server.js
+++ workoutlog/server/server.js
@@ -3,3 +3,11 @@ const path = require('path')
 
 const app = express();
 const port = process.env.port || 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).json({title: "Hello World"})
+})
+
+app.listen(port, () => {
+    console.log(`The server is listening on port ${port}`)
+})
