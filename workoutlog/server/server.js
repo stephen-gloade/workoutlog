07e6b1ddbdf2c30ee1cc63f10c5795c73e5bff54
Stephen <stephen.gloade@gmail.com>
2023-09-18 21:31:18 -0400

    mongodb connected, environment variables

--- workoutlog/server/server.js
+++ workoutlog/server/server.js
@@ -1,19 +1,39 @@
 const express = require('express')
 const path = require('path')
-require('dotenv').config(); //Can I set this to a variable?
+require('dotenv').config();
 
+const uri = process.env.MONGO_URI
 
 const app = express();
 const port = process.env.port || 4000;
 
 const { MongoClient } = require("mongodb");
 
-const client = new MongoClient(process.env.MONGO_URI)
+const client = new MongoClient(uri)
 
-app.get('/api/data', (req, res) => {
-    res.status(200).json({title: "Hello World", code: 200})
-})
+async function main() {
 
-app.listen(port, () => {
-    console.log(`The server is listening on port ${port}`)
-})
+    try {
+        await client.connect();
+        console.log('Connected to database');
+    } catch(e) {
+        console.error(e);
+    }
+
+}
+
+main()
+.catch(console.error)
+.finally(() => {
+    app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+    });
+});
+
+// app.get('/api/data', (req, res) => {
+//     res.status(200).json({title: "Hello World", code: 200})
+// })
+
+// app.listen(port, () => {
+//     console.log(`The server is listening on port ${port}`)
+// })
