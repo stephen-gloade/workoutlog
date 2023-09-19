94b534d11a9d7a6cd98a9dc0ca5a6762ea276eea
Stephen <stephen.gloade@gmail.com>
2023-09-18 21:17:22 -0400

    MongoDB

--- workoutlog/server/server.js
+++ workoutlog/server/server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const path = require('path')
+require('dotenv').config(); //Can I set this to a variable?
+
 
 const app = express();
 const port = process.env.port || 4000;
+
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGO_URI)
 
 app.get('/api/data', (req, res) => {
     res.status(200).json({title: "Hello World", code: 200})
